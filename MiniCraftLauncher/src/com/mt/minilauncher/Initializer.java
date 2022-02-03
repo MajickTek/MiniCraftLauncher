e1b5579e8c90028414bc1f72c4dce4f8f855863f
kennedyc5ATWIT <kingtmw2@gmail.com>
2022-02-03 00:49:33 -0500

    Temporarily remove user index

--- MiniCraftLauncher/src/com/mt/minilauncher/Initializer.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/Initializer.java
@@ -12,7 +12,7 @@ public class Initializer {
 	public static Path jarPath = Paths.get(launcherPath.toString(), "versions");
 	public static Path indexPath = Paths.get(launcherPath.toString(), "index");
 	
-	public static Path userIndexFile = Paths.get(launcherPath.toString(), "indexes.properties");
+	
 	public static void touchFoldersAndFiles() {
 		File f = launcherPath.toFile();
 		f.mkdirs();
@@ -26,29 +26,6 @@ public class Initializer {
 		f = indexPath.toFile();
 		f.mkdirs();
 		
-		f = userIndexFile.toFile();
-		try {
-			f.createNewFile();
-		} catch (IOException e) {
-			Debug.callCrashDialog("ERROR", "Something happened while creating the index db.\nCheck the console output.", Debug.ERR);
-			e.printStackTrace();
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("#The format is very simple.\n");
-		sb.append("#<index> = <version string>,<url>\n");
-		sb.append("#For example:\n");
-		sb.append("#0 = 1.0,https://www.download.com/my.jar\n");
-		sb.append("#Make sure it's a direct download link (you see a `.jar` at the end).\n");
-		sb.append("#Feel free to delete these comments.");
-		try {
-			FileWriter specWriter = new FileWriter(userIndexFile.toString());
-			specWriter.write(sb.toString());
-			specWriter.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 	public static void cleanFolders() {
