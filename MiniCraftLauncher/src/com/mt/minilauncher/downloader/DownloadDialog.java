2627507942b23d3afdd7d9829ff4ae80f4268a64
Majick Tek <kingtmw2@gmail.com>
2022-02-10 18:49:05 -0500

    finish adding  library and test downloader

--- MiniCraftLauncher/src/com/mt/minilauncher/downloader/DownloadDialog.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/downloader/DownloadDialog.java
@@ -4,14 +4,20 @@ import java.awt.EventQueue;
 
 import javax.swing.JDialog;
 import javax.swing.JProgressBar;
-import java.awt.BorderLayout;
 
-public class DownloadDialog extends JDialog {
+import com.littlebigberry.httpfiledownloader.FileDownloader;
+import com.littlebigberry.httpfiledownloader.FileDownloaderDelegate;
+
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+
+public class DownloadDialog extends JDialog implements FileDownloaderDelegate{
+	private JLabel progress;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -31,10 +37,38 @@ public class DownloadDialog extends JDialog {
 	public DownloadDialog() {
 		setTitle("Downloading");
 		setBounds(100, 100, 450, 100);
-		
-		JProgressBar progressBar = new JProgressBar();
-		getContentPane().add(progressBar, BorderLayout.CENTER);
 
+		FileDownloader fileDownloader = new FileDownloader(this);
+		
+		progress = new JLabel("0");
+		getContentPane().add(progress, BorderLayout.CENTER);
+		
+		fileDownloader.setUrl("https://github.com/MinicraftPlus/minicraft-plus-revived/releases/download/v2.0.7/minicraft_plus-2.0.7.jar");
+		fileDownloader.setLocalLocation("C:/Users/%username%/Documents/MajickTek/minilauncher/game.jar");
+		fileDownloader.beginDownload();
 	}
 
+	@Override
+	public void didStartDownload(FileDownloader fileDownloader) {
+		System.out.println("Begin");
+	}
+
+	@Override
+	public void didProgressDownload(FileDownloader fileDownloader) {
+		progress.setText(fileDownloader.getPercentComplete());
+	}
+
+	@Override
+	public void didFinishDownload(FileDownloader fileDownloader) {
+		progress.setText("done");
+	}
+
+	@Override
+	public void didFailDownload(FileDownloader fileDownloader) {
+		progress.setText("oops");
+	}
+
+	public JLabel getProgress() {
+		return progress;
+	}
 }
