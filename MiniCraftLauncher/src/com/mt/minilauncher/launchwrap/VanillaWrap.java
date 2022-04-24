e5bd0fd4f3f7c998f3f12f60fccb9efb86cf0173
Majick Tek <kingtmw2@gmail.com>
2022-04-24 01:16:09 -0400

    launcher hiding

--- MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/VanillaWrap.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/VanillaWrap.java
@@ -13,6 +13,18 @@ public class VanillaWrap implements IWrap{
 		String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
 		try {
 			Process ps = Runtime.getRuntime().exec(new String[] {"java", "-jar", path, "--savedir", vPath});
+			
+			//hide launcher
+			window.frmLauncher.setVisible(false);
+			try {
+				ps.waitFor();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			window.frmLauncher.setVisible(true);
+			//end hide launcher
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
