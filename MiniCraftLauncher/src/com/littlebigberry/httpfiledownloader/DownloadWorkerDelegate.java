71a589374ed9bf3d482cfc3bc66456bf9373f715
Majick Tek <kingtmw2@gmail.com>
2022-04-23 21:43:26 -0400

    Make filedownloader external

--- MiniCraftLauncher/src/com/littlebigberry/httpfiledownloader/DownloadWorkerDelegate.java
+++
@@ -1,8 +0,0 @@
-package com.littlebigberry.httpfiledownloader;
-
-public interface DownloadWorkerDelegate {
-	public void  didStartDownload(DownloaderWorker downloaderWorker);
-	public void    didReceiveData(DownloaderWorker downloaderWorker, byte[] data, int byteRead);
-	public void didFinishDownload(DownloaderWorker downloaderWorker);
-	public void   didFailDownload(DownloaderWorker downloaderWorker);
-}
