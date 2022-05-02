22886334da20037e259d66a7451459c4b2d0d979
Majick Tek <kingtmw2@gmail.com>
2022-05-02 19:00:42 -0400

    push beginnings of Fabric

--- MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/FabricWrap.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/FabricWrap.java
@@ -1,12 +1,28 @@
 package com.mt.minilauncher.launchwrap;
 
+import java.nio.file.Paths;
+
+import com.mt.minilauncher.Initializer;
 import com.mt.minilauncher.LauncherWindow;
+
+import net.fabricmc.loader.launch.knot.KnotClient;
 
 public class FabricWrap implements IWrap{
 
 	@Override
 	public void launchJar(String path, String version, LauncherWindow window) {
-		System.err.println(String.format("[%s] Not yet implemented.", this.getClass().getSimpleName()));
+		String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
+		
+		Thread gameThread = new Thread() {
+			@Override
+			public void run() {
+				System.setProperty("fabric.skipMcProvider", "true");
+				System.setProperty("fabric.gameJarPath", path);
+				KnotClient.main(new String[] {"--savedir", vPath});
+			}
+		};
+		gameThread.start();
+		
 	}
 
 }
