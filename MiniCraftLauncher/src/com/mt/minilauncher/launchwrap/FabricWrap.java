7a4feafbb59602283d9e78d6903fcf431134d2a7
Majick Tek <kingtmw2@gmail.com>
2022-05-03 22:42:42 -0400

    Vanilla/Fabric threaded
    Both launch wrappers now run in separate threads to allow running multiple versions

--- MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/FabricWrap.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/FabricWrap.java
@@ -13,38 +13,44 @@ public class FabricWrap implements IWrap{
 	@Override
 	public void launchJar(String path, String version, LauncherWindow window) {
 
-		String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
-		String lPath = Initializer.launcherPath.toString();
-		File f = new File("FabricBootstrap.jar");
-		if(!f.exists()) {
-			window.getConsole().setText("Failed to use FabricWrap, could not find FabricBootstrap.jar!");
-		} else {
-			window.getConsole().setText("Found FabricBootstrap.jar! Attempting to launch Fabric. Assuming the parameters have not changed.");
-			try {
-				//FabricBootstrap expects 3 arguments: the version folder, the jar file, and the launcher folder
-				Process ps = Runtime.getRuntime().exec(new String[] {"java", "-jar", "FabricBootstrap.jar", vPath, path, lPath});
-				
-				if(window.getHideLauncherMenuItem().isSelected()) {
-					window.frmLauncher.setVisible(false);
-				}
-				
-				try {
-					ps.waitFor();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				if(!window.frmLauncher.isVisible()) {
-					window.frmLauncher.setVisible(true);
-				}
-				
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+		Thread gameThread = new Thread() {
+			@Override
+			public void run() {
+				String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
+				String lPath = Initializer.launcherPath.toString();
+				File f = new File("FabricBootstrap.jar");
+				if(!f.exists()) {
+					window.getConsole().setText("Failed to use FabricWrap, could not find FabricBootstrap.jar!");
+				} else {
+					window.getConsole().setText("Found FabricBootstrap.jar! Attempting to launch Fabric. Assuming the parameters have not changed.");
+					try {
+						//FabricBootstrap expects 3 arguments: the version folder, the jar file, and the launcher folder
+						Process ps = Runtime.getRuntime().exec(new String[] {"java", "-jar", "FabricBootstrap.jar", vPath, path, lPath});
+						
+						if(window.getHideLauncherMenuItem().isSelected()) {
+							window.frmLauncher.setVisible(false);
+						}
+						
+						try {
+							ps.waitFor();
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						
+						if(!window.frmLauncher.isVisible()) {
+							window.frmLauncher.setVisible(true);
+						}
+						
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}			}
+		};
+		gameThread.start();
+		
 		
 	}
 
--- MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/VanillaWrap.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/VanillaWrap.java
@@ -10,29 +10,38 @@ public class VanillaWrap implements IWrap{
 
 	@Override
 	public void launchJar(String path, String version, LauncherWindow window) {
-		String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
-		try {
-			Process ps = Runtime.getRuntime().exec(new String[] {"java", "-jar", path, "--savedir", vPath});
-			
-			if(window.getHideLauncherMenuItem().isSelected()) {
-				window.frmLauncher.setVisible(false);
+
+		Thread gameThread = new Thread() {
+			@Override
+			public void run() {
+				String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
+				try {
+					Process ps = Runtime.getRuntime().exec(new String[] {"java", "-jar", path, "--savedir", vPath});
+					
+					if(window.getHideLauncherMenuItem().isSelected()) {
+						window.frmLauncher.setVisible(false);
+					}
+					
+					try {
+						ps.waitFor();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					if(!window.frmLauncher.isVisible()) {
+						window.frmLauncher.setVisible(true);
+					}
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			
-			try {
-				ps.waitFor();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			if(!window.frmLauncher.isVisible()) {
-				window.frmLauncher.setVisible(true);
-			}
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		};
+		gameThread.start();
+		
+		
 	}
 
 }
