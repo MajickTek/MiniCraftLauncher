8b98a39b94aa4a0bebc2e35902a88b0c478c0952
Majick Tek <kingtmw2@gmail.com>
2022-05-03 21:37:23 -0400

    Fix external Fabric handling

--- MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/FabricWrap.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/launchwrap/FabricWrap.java
@@ -1,5 +1,7 @@
 package com.mt.minilauncher.launchwrap;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 
 import com.mt.minilauncher.Initializer;
@@ -10,17 +12,32 @@ public class FabricWrap implements IWrap{
 
 	@Override
 	public void launchJar(String path, String version, LauncherWindow window) {
-//		String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
-//		String lPath = Initializer.launcherPath.toString();
-//		Thread gameThread = new Thread() {
-//			@Override
-//			public void run() {
-//				System.setProperty("fabric.skipMcProvider", "true");
-//				System.setProperty("fabric.gameJarPath", path);
-//				KnotClient.main(new String[] {"--gameDir", lPath, "--savedir", vPath});
-//			}
-//		};
-//		gameThread.start();
+
+		String vPath = Paths.get(Initializer.savesDir.toString(), version).toString();
+		String lPath = Initializer.launcherPath.toString();
+		File f = new File("FabricBootstrap.jar");
+		if(!f.exists()) {
+			window.getConsole().setText("Failed to use FabricWrap, could not find FabricBootstrap.jar!");
+		} else {
+			try {
+				Process ps = Runtime.getRuntime().exec(new String[] {"java", "-jar", "FabricBootstrap.jar", vPath, path, lPath});
+				
+				//hide launcher
+				window.frmLauncher.setVisible(false);
+				try {
+					ps.waitFor();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				window.frmLauncher.setVisible(true);
+				//end hide launcher
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
