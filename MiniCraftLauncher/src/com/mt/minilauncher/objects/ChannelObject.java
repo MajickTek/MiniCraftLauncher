8982691e7dfad1729dbc0ad3e37c2ccf1dd957d0
Majick Tek <kingtmw2@gmail.com>
2022-05-04 23:22:17 -0400

    Add XML parsing for ChannelSelector

--- MiniCraftLauncher/src/com/mt/minilauncher/objects/ChannelObject.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/objects/ChannelObject.java
@@ -1,6 +1,7 @@
 package com.mt.minilauncher.objects;
 
 import java.io.Serializable;
+import java.util.Random;
 
 public class ChannelObject implements Serializable{
 	/**
@@ -8,14 +9,21 @@ public class ChannelObject implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	public String channelFile;
+	public String channelName;
 	
 	public ChannelObject(String channelFile) {
+		Random rand = new Random();
 		this.channelFile = channelFile;
+		this.channelName = "Channel " + rand.nextInt(0, 256);
 	}
 
+	public ChannelObject(String channelFile, String channelName) {
+		this.channelFile = channelFile;
+		this.channelName = channelName;
+	}
 	@Override
 	public String toString() {
-		return channelFile;
+		return channelName;
 	}
 	
 	
