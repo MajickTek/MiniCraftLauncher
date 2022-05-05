ed26b9db65b7cd210a7538c96b365248fa0b9d92
Majick Tek <kingtmw2@gmail.com>
2022-05-04 22:09:48 -0400

    Add isRunning to VersionObject

--- MiniCraftLauncher/src/com/mt/minilauncher/objects/VersionObject.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/objects/VersionObject.java
@@ -15,25 +15,38 @@ public class VersionObject implements Serializable {
 	
 	public boolean canEdit;
 	
+	public boolean isRunning;
+	
 	public VersionObject(String url, String version) {
 		this.url = url;
 		this.version = version;
-		isDownloaded = false;
-		canEdit = false;
+		this.isDownloaded = false;
+		this.canEdit = false;
+		this.isRunning = false;
 	}
 	
 	public VersionObject(String url, String version, boolean canEdit) {
 		this.url = url;
 		this.version = version;
-		isDownloaded = false;
+		this.isDownloaded = false;
 		this.canEdit = canEdit;
+		this.isRunning = false;
+	}
+	
+	public VersionObject(String url, String version, boolean canEdit, boolean isRunning) {
+		this.url = url;
+		this.version = version;
+		this.isDownloaded = false;
+		this.canEdit = canEdit;
+		this.isRunning = isRunning;
 	}
 	
 	public VersionObject() {
 		this.url = "UNKNOWN";
 		this.version = "UNKNOWN";
-		isDownloaded = false;
-		canEdit = true;
+		this.isDownloaded = false;
+		this.canEdit = true;
+		this.isRunning = false;
 	}
 
 	public String getURL() {
@@ -48,6 +61,14 @@ public class VersionObject implements Serializable {
 		return isDownloaded ? "Ready to launch!" : "Not yet downloaded.";
 	}
 	
+	public boolean isRunning() {
+		return isRunning;
+	}
+
+	public void setRunning(boolean isRunning) {
+		this.isRunning = isRunning;
+	}
+
 	@Override
 	public String toString() {
 		return String.format("%s [%s]", version, getDownloadStatus());
