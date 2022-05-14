377e065b1b001fd7728ffc9955ae392450108d8c
Majick Tek <kingtmw2@gmail.com>
2022-05-14 00:29:10 -0400

    Beginning of adding "live" xmls

--- MiniCraftLauncher/src/com/mt/minilauncher/objects/ChannelObject.java
+++ MiniCraftLauncher/src/com/mt/minilauncher/objects/ChannelObject.java
@@ -11,12 +11,14 @@ public class ChannelObject implements Serializable{
 	public String channelFile;
 	public String channelName;
 	public String target;
+	boolean isLive; // use dynamically-generated XML
 	
 	public ChannelObject(String channelFile) {
 		Random rand = new Random();
 		this.channelFile = channelFile;
 		this.channelName = "Channel " + rand.nextInt(0, 256);
 		this.target = "index" + rand.nextInt(0, 256) + ".xml";
+		isLive = false;
 	}
 
 	public ChannelObject(String channelFile, String channelName) {
@@ -24,13 +26,49 @@ public class ChannelObject implements Serializable{
 		this.channelFile = channelFile;
 		this.channelName = channelName;
 		this.target = "index" + rand.nextInt(0, 256) + ".xml";
+		isLive = false;
 	}
 	
 	public ChannelObject(String channelFile, String channelName, String target) {
 		this.channelFile = channelFile;
 		this.channelName = channelName;
 		this.target = target;
+		isLive = false;
 	}
+	
+	
+	public String getChannelFile() {
+		return channelFile;
+	}
+
+	public void setChannelFile(String channelFile) {
+		this.channelFile = channelFile;
+	}
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+	public boolean isLive() {
+		return isLive;
+	}
+
+	public void setLive(boolean isLive) {
+		this.isLive = isLive;
+	}
+
 	@Override
 	public String toString() {
 		return channelName;
